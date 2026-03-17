cf555174693172dcab3deb1fa26e69388de6bf89
yahia008 <120284117+yahia008@users.noreply.github.com>
2026-03-17 10:19:14 +0000

    controller

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user/user.service';
 import { UserService } from './user.service';
+import { UserController } from './user.controller';
 
 @Module({
-  providers: [UserService]
+  providers: [UserService],
+  controllers: [UserController]
 })
 export class UserModule {}
