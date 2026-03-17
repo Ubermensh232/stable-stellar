b9c140ddf866112914ea80cc40f2ed7df0f1413d
yahia008 <120284117+yahia008@users.noreply.github.com>
2026-03-17 13:32:26 +0000

    hello worl

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'welcome to stable-stellar';
+    return 'Hello World!';
   }
 }
